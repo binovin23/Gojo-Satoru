77e434d4e9624a873dae5f1fa1de0f49e3a5e24b
binovin23 <110671931+binovin23@users.noreply.github.com>
2022-08-16 02:37:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94703509273']
+global.premium = ['94703509273']
+global.ownernomer = '94703509273'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
